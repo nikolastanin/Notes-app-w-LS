0f85fad4d6dc72d7e7fadf2159bc542bac235201
nikolastanin <nikolastanin@gmail.com>
2021-05-03 18:43:05 +0200

    adding notes with ls

--- main.js
+++ main.js
@@ -1,48 +1,35 @@
 let textInput = document.getElementById("textInput");
 let addButton = document.querySelector("#add");
 let notes = [];
-// let p = document.getElementById("pag");
-
+let  notesList = document.getElementById("notesList");
 
 //display note if there is something in Local Storage
 let fetchNote = () =>{
         console.log("radi");
         const fetchData = localStorage.getItem("array");
-       const fetchArray = JSON.parse(fetchData);
-      notes  = fetchArray;
-       console.log(fetchArray);
-    //   displayNote(notes);
-
+        const fetchArray = JSON.parse(fetchData);
+        notes  = fetchArray;
+         console.log(fetchArray);
+    
 }
 //ispis svih prethodih poruka
-let displayNote = () =>{
- 
-  
-    for( let i =0; i<notes.length; i++){
-        let x = document.createElement("p");
-        console.log(notes[i].message); 
-        x.innerHTML = notes[i].message;
-        document.body.appendChild(x);
-       console.log(i+"!!");
-   
-    }  
- 
-   
-  
-  
+let displayNote = (value) =>{
+    let noteElement = document.createElement("div");
+    console.log("prebacaj"+value);
+    noteElement.innerHTML = ` <p>${notes[value].message}</p>`;
+    notesList.appendChild(noteElement);
     
-        
 }
 
 
-
-//check if there is something in local storage
-
+//check if there is something in local storage and display if is
     if(localStorage.getItem("array")!==null){
-
         console.log("ima nesto");
         fetchNote();
-        displayNote();
+        for(let i =0; i<notes.length; i++){
+            displayNote(i);
+        }
+        
     }
     else{
         console.log("nema nista");
@@ -50,51 +37,43 @@ let displayNote = () =>{
     }
 
 
-
-
 //creating new note and sending data to setLocalStorage function
 const newNote = () =>{
-    
-    console.log("click");
-    let noteObj  = new Object();
-    let i ;
-    for( i = 0; i<=notes.length; i++){
-
+    if(textInput.value!=0 ){
+        console.log("click");
+        let noteObj  = new Object();
+        //ovo je je menjano 
+      
+        let i ;
         noteObj.id = i;
-        noteObj.delete = "onclick=delete"+i;
         noteObj.message = textInput.value;
-        // console.log(i);
+        notes.push(noteObj);
+        
+        for(i = 0; i<notes.length; i++){
+            
+            noteObj.id = i;
+            noteObj.delete = "onclick=delete"+i;
+          
+          console.log("ovo je vrednost i "+ i)
        
     }
-     notes.push(noteObj);
-     //ispis trenutno iskucane poruke
-  //i-1 because i goes up when out of loop
-     console.log(notes[i-1].message+"ovo je poruka ispisana od kor trenutno");
-    //  x.innerHTML = ` <p>${notes[i-1].message}</p>`
-    let y = document.createElement("p");
-    y.innerHTML = ` <p>${notes[i-1].message}</p>`;
-    document.body.appendChild(y);
-    
+    console.log("ovo je vrednost i van petlje"+ i)
+         // 
+        displayNote(notes.length-1);
         
-    
-    setLocalStorage();
-    
-    
-
+            setLocalStorage();
+    }
+    else
+    {
+        console.log("ispisi neki unis");
+    }
 }
 //Setting local storage data from user message
     const setLocalStorage = () =>{
-
         const stringifyArray = JSON.stringify(notes);
         localStorage.setItem("array",stringifyArray);
     }
-       
-     
-    
+
+    addButton.addEventListener("click",newNote);
 
 
-
-
-
-
-addButton.addEventListener("click",newNote);
