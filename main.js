8887fefb857fa81fd500794bc132c82712e40ce7
nikolastanin <nikolastanin@gmail.com>
2021-05-03 14:45:15 +0200

    fix

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 let textInput = document.getElementById("textInput");
 let addButton = document.querySelector("#add");
 let notes = [];
-let test = [{ name: "John", today: new Date(), city: "New York" },{ name: "John", today: new Date(), city: "New York" },{ name: "John", today: new Date(), city: "New York" }];
-let p = document.getElementById("pag");
+// let p = document.getElementById("pag");
 
 
 //display note if there is something in Local Storage
@@ -17,14 +16,19 @@ let fetchNote = () =>{
 }
 //ispis svih prethodih poruka
 let displayNote = () =>{
-    let i ;
-    let x = document.createElement("div");
-    for( i =0; i<notes.length; i++){
+ 
+  
+    for( let i =0; i<notes.length; i++){
+        let x = document.createElement("p");
         console.log(notes[i].message); 
-        x.innerHTML = ` <p>${notes[i].message}</p>` 
-        document.body.appendChild(x); 
+        x.innerHTML = notes[i].message;
+        document.body.appendChild(x);
+       console.log(i+"!!");
+   
     }  
-    
+ 
+   
+  
   
     
         
@@ -57,7 +61,7 @@ const newNote = () =>{
     for( i = 0; i<=notes.length; i++){
 
         noteObj.id = i;
-        noteObj.delete = "addFuncForDelete"+i;
+        noteObj.delete = "onclick=delete"+i;
         noteObj.message = textInput.value;
         // console.log(i);
        
@@ -67,7 +71,7 @@ const newNote = () =>{
   //i-1 because i goes up when out of loop
      console.log(notes[i-1].message+"ovo je poruka ispisana od kor trenutno");
     //  x.innerHTML = ` <p>${notes[i-1].message}</p>`
-    let y = document.createElement("div");
+    let y = document.createElement("p");
     y.innerHTML = ` <p>${notes[i-1].message}</p>`;
     document.body.appendChild(y);
     
